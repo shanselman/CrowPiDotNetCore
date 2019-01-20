ab2fb4309b80fb1b29e5d209453dbf6b5e2fd3e6
Scott Hanselman <scott@hanselman.com>
2019-01-19 17:05:30 -0800

    Tasks and Launch files for REMOTE debugging on a Pi

--- rpitest/publish.bat
+++ rpitest/publish.bat
@@ -1,4 +1,4 @@
 dotnet publish -r linux-arm /p:ShowLinkerSizeComparison=true 
 pushd .\bin\Debug\netcoreapp2.1\linux-arm\publish
-pscp -pw raspberry -v -r .\* pi@crowpi:/home/pi/Desktop/rpitest
+pscp -pw raspberry -v -r .\* pi@crowpi.lan:/home/pi/Desktop/rpitest
 popd
