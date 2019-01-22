6d56634386ef073408d0d2916c7ec20f69f62bad
Scott Hanselman <scott@hanselman.com>
2019-01-22 15:51:34 -0800

    moving to SSH and SCP over Putty

--- rpitest/publish.bat
+++ rpitest/publish.bat
@@ -1,6 +1,14 @@
 dotnet publish -r linux-arm /p:ShowLinkerSizeComparison=true 
-pushd .\bin\Debug\netcoreapp2.1\linux-arm\publish
+pushd .\bin\Debug\netcoreapp2.2\linux-arm\publish
+
 REM Publish the .NET Core Framework ONCE, then just move the EXE
+
+REM Putty 
 REM pscp -pw raspberry -v -r .\* pi@crowpi.lan:/home/pi/Desktop/rpitest
-pscp -pw raspberry -v -r .\rpitest pi@crowpi.lan:/home/pi/Desktop/rpitest
+REM pscp -pw raspberry -v -r .\rpitest pi@crowpi.lan:/home/pi/Desktop/rpitest
+
+REM Built-in OpenSSH
+REM scp -r .\* pi@crowpi.lan:/home/pi/Desktop/rpitest
+scp -r .\rpitest* pi@crowpi.lan:/home/pi/Desktop/rpitest
+
 popd
